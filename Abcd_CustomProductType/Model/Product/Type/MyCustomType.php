5b8b7d1d6350c1aaf9b57f05b9eb086b46b9c301
Beparr Bazaar <beaprr2021@gmail.com>
2026-05-01 15:15:23 +0530

    Initial commit: Add Abcd CustomProductType module

---
+++ Abcd_CustomProductType/Model/Product/Type/MyCustomType.php
@@ -0,0 +1,24 @@
+<?php
+namespace Abcd\CustomProductType\Model\Product\Type;
+
+use Magento\Catalog\Model\Product\Type\AbstractType;
+
+class MyCustomType extends AbstractType
+{
+    /**
+     * Product type code
+     */
+    const TYPE_CODE = 'my_custom_product';
+
+    /**
+     * Delete data specific for this product type
+     *
+     * @param \Magento\Catalog\Model\Product $product
+     * @return void
+     */
+    public function deleteTypeSpecificData(\Magento\Catalog\Model\Product $product)
+    {
+        // Add any specific logic needed when a product of this type is deleted.
+        // For a basic product type, this can remain empty.
+    }
+}
